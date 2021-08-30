d0d845cf7f5db8ab6be2e84369edc3275748cf3d
Andres Castro <andrescastro2011@hotmail.com>
2021-08-29 23:40:50 -0300

    incorporacion de menu

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+export const NavBar = () => {
+    return (
+        <nav> 
+            <a href ='#'>Store</a>
+            <a href ='#'>Inicio</a>
+            <a href ='#'>Zapatillas</a>
+            <a href ='#'>Remeras</a>
+            <a href ='#'>Pantalones</a>
+        </nav>
+    )
+}
+
