df7ff9e8df0172245beceb674d202e696feae2e3
Andres Castro <andrescastro2011@hotmail.com>
2021-09-09 19:32:58 -0300

    desafio contador con boton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ItemListContainer from './components/NavBar/ItemListContainer';
+import ItemListContainer from './components/ItemListContainer';
 import NavBar from './components/NavBar/NavBar'
 
 function App() {
   return (
     <>
     <NavBar/>
-    <ItemListContainer mensaje = 'Hola soy Item List Container'/>
+    <ItemListContainer/>
     </>
   );
   
---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,57 @@
+import {useState} from 'react'
+import  Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from'react-bootstrap/Row'
+
+const ItemCount = ({stock , initial , onAdd}) => {
+
+    const [count, setCount] = useState(initial)
+
+    function sumar() {
+        setCount( count + 1)
+    }
+        
+    function restar() {
+        if (count > 1){
+            setCount (count - 1)
+        }
+
+    }
+
+    const agregarCarrito = () => {
+        onAdd(count)
+    }
+
+   
+    return (
+        <>
+        <Container>
+            
+            <Row className = "justify-content-md-center">
+            <h2 >Productos</h2>
+            </Row>
+            
+            <Row className = "justify-content-md-center">
+            <label variant = "danger" size ="lg">{count}</label> <br />
+            </Row>
+            
+            <Row className = "justify-content-md-center">
+            <Button variant="danger" onClick ={sumar}>+</Button>{' '}
+            <Button variant="danger" onClick ={agregarCarrito}>Agregar al carrito</Button>{' '}
+            <Button variant="danger" onClick ={restar}>-</Button>{' '}
+            </Row>
+            
+        </Container>
+        </>
+    )
+}
+
+export default ItemCount
+
+   // <div>
+        //     <h2>Soy Item Count</h2>
+        //     <label>{count}</label> <br />
+        //     <button onClick = {sumar}>+</button>
+        //     <button onClick = {agregarCarrito}>Agregar al carrito</button>
+        //     <button onClick = {restar}>-</button>
+        // </div>
